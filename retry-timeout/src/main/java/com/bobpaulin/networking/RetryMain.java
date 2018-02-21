41fbd10350bb167406c816c07014f0001aa1712f
bpaulin <bpaulin@bpaulin-VirtualBox>
2018-02-20 21:28:57 -0600

    Update Retry

--- retry-timeout/src/main/java/com/bobpaulin/networking/RetryMain.java
+++ retry-timeout/src/main/java/com/bobpaulin/networking/RetryMain.java
@@ -3,10 +3,12 @@ package com.bobpaulin.networking;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http4.HttpComponent;
+import org.apache.camel.component.netty4.http.NettyHttpComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.model.rest.RestBindingMode;
 
-public class Main {
+public class RetryMain {
 
 	public static void main(String[] args) throws Exception {
 		
@@ -20,10 +22,17 @@ public class Main {
 
 				restConfiguration().component("netty4-http").port(8282).bindingMode(RestBindingMode.json);
 				
+				NettyHttpComponent netty4Component = getContext().getComponent("netty4-http", NettyHttpComponent.class);
+				netty4Component.setMaximumPoolSize(1000);
+				
+				HttpComponent httpComponent = getContext().getComponent("http4", HttpComponent.class);
+				httpComponent.setConnectionsPerRoute(1000);
+				httpComponent.setMaxTotalConnections(1000);
 				
 				rest("/timeout").get().to("direct:timeout");
 				rest("/retry").get().to("direct:retry");
 				rest("/notimeout").get().to("direct:notimeout");
+				rest("/hystrix").get().to("direct:hystrix");
 				
 				from("direct:notimeout")
 				.setHeader(Exchange.HTTP_METHOD, constant("GET"))
@@ -33,7 +42,7 @@ public class Main {
 				from("direct:timeout")
 				.setHeader(Exchange.HTTP_METHOD, constant("GET"))
                 .setBody(simple("null"))
-                .to("http4://localhost:8888/threads/test?bridgeEndpoint=true&throwExceptionOnFailure=false&httpClient.socketTimeout=600");
+                .to("http4://localhost:8888/threads/test?bridgeEndpoint=true&throwExceptionOnFailure=false&httpClient.socketTimeout=1500");
 				
 				from("direct:retry")
 					.onException(Exception.class)
@@ -46,6 +55,18 @@ public class Main {
 					.setHeader(Exchange.HTTP_METHOD, constant("GET"))
 	                .setBody(simple("null"))
 	                .to("http4://localhost:8888/threads/test?bridgeEndpoint=true&throwExceptionOnFailure=false&httpClient.socketTimeout=1500");
+				
+				from("direct:hystrix")
+					.hystrix()
+				        .hystrixConfiguration()
+				             .executionTimeoutInMilliseconds(2000)
+				             .queueSizeRejectionThreshold(100)
+				             .corePoolSize(200)
+				        .end()
+				        .to("http4://localhost:8888/threads/test?bridgeEndpoint=true&throwExceptionOnFailure=false")
+					    .onFallback()
+					        .transform().constant("Fallback message")
+					    .end();
 				
 			}
 		});
